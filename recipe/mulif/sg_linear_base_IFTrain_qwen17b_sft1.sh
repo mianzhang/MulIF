7c47347c772454073b2c967fd033023287a22398
mianzhang <gugumian@gmail.com>
2026-04-18 11:39:05 -0500

    update

--- recipe/mulif/sg_linear_base_IFTrain_qwen17b_sft1.sh
+++ recipe/mulif/sg_linear_base_IFTrain_qwen17b_sft1.sh
@@ -48,7 +48,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0.0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='MulIF' \
-    trainer.experiment_name='sg_linear_base_IFTrain_qwen17b_sft1' \
+    trainer.experiment_name='IFTrain_linear_base_qwen17b_sft1' \
     trainer.n_gpus_per_node=$n_gpus_per_node \
     trainer.nnodes=1 \
     trainer.save_freq=25 \
@@ -58,6 +58,6 @@ python3 -m verl.trainer.main_ppo \
     trainer.resume_from_path=null \
     trainer.test_freq=25 \
     trainer.total_epochs=10 \
-    trainer.total_training_steps=500 > log/sg_linear_base_IFTrain_qwen17b_sft1.log
+    trainer.total_training_steps=500 > log/IFTrain_linear_base_qwen17b_sft1.log
 
     # actor_rollout_ref.actor.fsdp_config.model_dtype=bfloat16 \
