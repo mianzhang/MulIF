4e3d24ca21bcc4aada3f58250f9d219b9abd2f82
yinyueqin <yinyueqinoni@163.com>
2026-04-25 14:30:52 +0000

    update amlt job to llama8b with set -ex tracing and tune training params

--- recipe/mulif_ms/IFTrain_baseline_llama8b.sh
+++ recipe/mulif_ms/IFTrain_baseline_llama8b.sh
@@ -52,7 +52,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -67,14 +67,14 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=16 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.ref.fsdp_config.param_offload=False \
     actor_rollout_ref.ref.strategy=fsdp2 \
     algorithm.use_kl_in_reward=False \
     reward_model.reward_manager=ifverify_sg_score \
-    reward_model.launch_reward_fn_async=False \
+    reward_model.launch_reward_fn_async=True \
     +reward_model.reward_kwargs.inst_weight_mode=none \
     trainer.critic_warmup=0.0 \
     trainer.logger=['console','wandb'] \
