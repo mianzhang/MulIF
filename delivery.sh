b9b6d6cd2916bbaaf096c223cddc2e7596b27fe8
mianzhang <gugumian@gmail.com>
2026-04-26 20:26:34 -0500

    update

--- delivery.sh
+++ delivery.sh
@@ -1,11 +1,12 @@
-python -m verl.model_merger merge \
-    --backend fsdp \
-    --local_dir checkpoints/IFTrain_noconf_baseline_qwen17b/global_step_500/actor/ \
-    --target_dir hf_cache/IFTrain_noconf_baseline_qwen17b_step500
-python hf_upload.py hf_cache/IFTrain_noconf_baseline_qwen17b_step500 --repo-id billmianz/IFTrain_noconf_baseline_qwen17b_step500 --repo-type model
 
+
+for model in IFTrain_baseline_qwen17b IFTrain_linear_base_qwen17b IFTrain_baseline_qwen17b_sft1 IFTrain_linear_base_qwen17b_sft1; do
+
+for step in 100 200 300 400; do
 python -m verl.model_merger merge \
     --backend fsdp \
-    --local_dir checkpoints/IFTrain_noconf_linear_base_qwen17b/global_step_500/actor/ \
-    --target_dir hf_cache/IFTrain_noconf_linear_base_qwen17b_step500
-python hf_upload.py hf_cache/IFTrain_noconf_linear_base_qwen17b_step500 --repo-id billmianz/IFTrain_noconf_linear_base_qwen17b_step500 --repo-type model
+    --local_dir checkpoints/${model}/global_step_${step}/actor/ \
+    --target_dir hf_cache/${model}_step${step}
+python hf_upload.py hf_cache/${model}_step${step} --repo-id billmianz/${model}_step${step} --repo-type model
+done
+done
