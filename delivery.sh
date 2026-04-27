db80e80168ba3964001af0141c47864e428a008f
yinyueqin <yinyueqinoni@163.com>
2026-04-27 04:20:52 +0000

    update delivery and train configs: consolidate delivery job, switch to qwen17b_sft1_gz64, fix storage paths

--- delivery.sh
+++ delivery.sh
@@ -5,8 +5,8 @@ for model in IFTrain_baseline_qwen17b IFTrain_linear_base_qwen17b IFTrain_baseli
 for step in 100 200 300 400; do
 python -m verl.model_merger merge \
     --backend fsdp \
-    --local_dir checkpoints/${model}/global_step_${step}/actor/ \
-    --target_dir hf_cache/${model}_step${step}
-python hf_upload.py hf_cache/${model}_step${step} --repo-id billmianz/${model}_step${step} --repo-type model
+    --local_dir ${AZURE_STORAGE_ROOT}/checkpoints/${model}/global_step_${step}/actor/ \
+    --target_dir ${AZURE_STORAGE_ROOT}/hf_cache/${model}_step${step}
+python hf_upload.py ${AZURE_STORAGE_ROOT}/hf_cache/${model}_step${step} --repo-id billmianz/${model}_step${step} --repo-type model
 done
 done
