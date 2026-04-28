dc0108a93287dcd4f96f231767119d831a139fd8
mianzhang <gugumian@gmail.com>
2026-04-27 21:56:35 -0500

    larger_gz

--- hf_model_download.py
+++ hf_model_download.py
@@ -19,21 +19,22 @@ if model_cache_dir is None:
     model_cache_dir = str(Path(default_storage_root) / "hf_cache") if default_storage_root else "hf_cache"
 
 model_pool = [
-    # 'Qwen/Qwen3-0.6B',
-    # 'Qwen/Qwen3-1.7B',
-    # 'billmianz/sg_baseline_rule_qwen17b_step125'
-    # 'billmianz/IFTrain_baseline_qwen17b_step500'
-    # 'billmianz/qwen17b_sft1'
-    # 'billmianz/qwen17b_sft2'
-    # 'billmianz/qwen17b_mh_sft1'
-    'meta-llama/Llama-3.1-8B-Instruct',
-    'billmianz/llama8b_sft1'
-    # 'billmianz/IFTrain_noconf_baseline_qwen17b_step500'
-    # 'billmianz/IFTrain_baseline_qwen17b_sft1_step200',
-    # 'billmianz/IFTrain_baseline_qwen17b_sft1_step500'
-    # 'billmianz/IFTrain_linear_base_qwen17b_sft1_step200'
-    # 'Qwen/Qwen3-4B',
-    # 'Qwen/Qwen3-8B'
+    # "billmianz/IFTrain_baseline_qwen17b_step100",
+    # "billmianz/IFTrain_baseline_qwen17b_step200",
+    # "billmianz/IFTrain_baseline_qwen17b_step300",
+    # "billmianz/IFTrain_baseline_qwen17b_step400",
+    # "billmianz/IFTrain_baseline_qwen17b_sft1_step100",
+    # "billmianz/IFTrain_baseline_qwen17b_sft1_step200",
+    # "billmianz/IFTrain_baseline_qwen17b_sft1_step300",
+    # "billmianz/IFTrain_baseline_qwen17b_sft1_step400",
+    # "billmianz/IFTrain_linear_base_qwen17b_sft1_step100",
+    # "billmianz/IFTrain_linear_base_qwen17b_sft1_step200",
+    # "billmianz/IFTrain_linear_base_qwen17b_sft1_step300",
+    # "billmianz/IFTrain_linear_base_qwen17b_sft1_step400",
+    # "billmianz/IFTrain_linear_base_qwen17b_step100",
+    # "billmianz/IFTrain_linear_base_qwen17b_step200",
+    # "billmianz/IFTrain_linear_base_qwen17b_step300",
+    # "billmianz/IFTrain_linear_base_qwen17b_step400",
     ]
 
 for repo_id in model_pool:
