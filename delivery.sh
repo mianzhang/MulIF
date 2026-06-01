8a108779b7b927162b6edb3c609ee41b5f0e35e7
mianzhang <gugumian@gmail.com>
2026-05-31 21:45:37 -0500

    update

--- delivery.sh
+++ delivery.sh
@@ -1,19 +1,8 @@
 
 
-for model in stage2_baseline_qwen7b_sft_mh_100; do
+for model in stage2_data_ablation_qwen17b_sft_mh_100 stage2_gz8_baseline_qwen17b stage2_gz8_linear_base_pairwise_alpha2_qwen7b_sft_mh_100 stage2_gz32_baseline_qwen17b stage2_gz32_linear_base_pairwise_alpha2_qwen7b_sft_mh_100; do
 
-for step in 100 200 300 400; do
-python -m verl.model_merger merge \
-    --backend fsdp \
-    --local_dir ${AZURE_STORAGE_ROOT}/checkpoints/${model}/global_step_${step}/actor/ \
-    --target_dir ${AZURE_STORAGE_ROOT}/hf_cache/${model}_step${step}
-python hf_upload.py ${AZURE_STORAGE_ROOT}/hf_cache/${model}_step${step} --repo-id billmianz/${model}_step${step} --repo-type model
-done
-done
-
-for model in stage2_linear_base_qwen7b_sft_mh_100; do
-
-for step in 300 400; do
+for step in 200 300; do
 python -m verl.model_merger merge \
     --backend fsdp \
     --local_dir ${AZURE_STORAGE_ROOT}/checkpoints/${model}/global_step_${step}/actor/ \
